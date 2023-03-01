6866c319c41b856b4b170119b4f934cf6ababf05
Inu John <inuoshios@gmail.com>
2023-03-01 11:52:00 +0100

    fix: add build

---
+++ build/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const config_1 = __importDefault(require("./config"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "push notification test" });
+});
+app.listen(config_1.default.port, () => {
+    console.info(`app is running on port: ${config_1.default.port}`);
+});
